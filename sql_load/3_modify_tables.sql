fe6c291aa9c7aa260dea3ed95d445d5813db42ee
dhoff186 <daniel.hoffman186@gmail.com>
2025-12-17 16:31:58 -0500

    first commit

---
+++ sql_load/3_modify_tables.sql
@@ -0,0 +1,20 @@
+COPY company_dim
+FROM 'C:\hoffm\sql_analysis\csv_files\company_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY skills_dim
+FROM 'C:\hoffm\sql_analysis\csv_files\skills_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY job_postings_fact
+FROM 'C:\hoffm\sql_analysis\csv_files\job_postings_fact.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY skills_job_dim
+FROM 'C:\hoffm\sql_analysis\csv_files\skills_job_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+
+
+select * from job_postings_fact
+limit 100;
