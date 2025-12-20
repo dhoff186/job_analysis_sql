295a56759009aaf4d2f751a8cb4e043966d7d42e
dhoff186 <daniel.hoffman186@gmail.com>
2025-12-20 03:21:03 -0500

    minor fix

--- top_jobs/4_top_paying_skills.sql
+++ top_jobs/4_top_paying_skills.sql
@@ -7,7 +7,6 @@ INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
 WHERE
     job_title_short = 'Data Scientist' 
     AND salary_year_avg IS NOT NULL
-    --AND job_location = 'United States'
 GROUP BY
     skills
    
